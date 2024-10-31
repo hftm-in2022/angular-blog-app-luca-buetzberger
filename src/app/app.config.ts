a8cb52b7e9f48fee87a64bab46de8e188bc07b03
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-10-31 22:13:55 +0100

    feat(firebase): add firebase to angular project and configure connection data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,7 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import {
-  initializeAppCheck,
-  ReCaptchaEnterpriseProvider,
-  provideAppCheck,
-} from '@angular/fire/app-check';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
@@ -24,24 +18,24 @@ export const appConfig: ApplicationConfig = {
         projectId: 'ldb-flutter-blog',
         appId: '1:177083748320:web:ac7e1c16c96703f15ea585',
         storageBucket: 'ldb-flutter-blog.appspot.com',
-        locationId: 'europe-west',
+        // locationId: 'europe-west',
         apiKey: 'AIzaSyAp_vfzMBwBl54-p7GwuMhfZ7w8mtSW0q8',
         authDomain: 'ldb-flutter-blog.firebaseapp.com',
         messagingSenderId: '177083748320',
       }),
     ),
     provideAuth(() => getAuth()),
-    provideAppCheck(() => {
-      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-      const provider =
-        new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
-      return initializeAppCheck(undefined, {
-        provider,
-        isTokenAutoRefreshEnabled: true,
-      });
-    }),
+    // provideAppCheck(() => {
+    //   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+    //   const provider =
+    //     new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+    //   return initializeAppCheck(undefined, {
+    //     provider,
+    //     isTokenAutoRefreshEnabled: true,
+    //   });
+    // }),
     provideFirestore(() => getFirestore()),
-    provideMessaging(() => getMessaging()),
+    // provideMessaging(() => getMessaging()),
     provideStorage(() => getStorage()),
   ],
 };
