1eccae60709782bb29688fa34b336c29ba779752
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-12-01 19:15:39 +0100

    feat(blog-overview): move blog overview code from app component to features blog-overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { BlogOverviewComponent } from './features/blog-overview/blog-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: BlogOverviewComponent }];
---
+++ src/app/features/blog-overview/blog-overview.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogService } from '../../services/blog.service';
+import { BlogPost } from '../../models/blogpost.model';
+
+@Component({
+  selector: 'app-blog-overview',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './blog-overview.component.html',
+  styleUrls: ['./blog-overview.component.scss'],
+})
+export class BlogOverviewComponent implements OnInit {
+  blogs: BlogPost[] = [];
+  loading = true;
+
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit() {
+    this.loadBlogs();
+  }
+
+  loadBlogs() {
+    this.loading = true;
+    this.blogService.getBlogs().subscribe({
+      next: (blogs) => {
+        this.blogs = blogs;
+        this.loading = false;
+      },
+    });
+  }
+}
