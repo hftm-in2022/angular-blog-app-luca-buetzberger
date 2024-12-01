09db45c6e9ed2a6ee2b328c8feac579b6f7baf7c
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-12-01 21:34:58 +0100

    feat: add old demo page back

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { BlogOverviewComponent } from './features/blog-overview/blog-overview.component';
+import { DemoPageComponent } from './features/demo-page/demo-page.component';
 
-export const routes: Routes = [{ path: '', component: BlogOverviewComponent }];
+export const routes: Routes = [
+  { path: '', component: BlogOverviewComponent },
+  { path: 'demo', component: DemoPageComponent },
+];
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss',
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent {}
