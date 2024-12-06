b03d1cebadfa399b92ab86481870f43ce2cb5421
Luca Dario Bützberger <74469324+TukaWarrior@users.noreply.github.com>
2024-12-06 16:57:19 +0100

    feat(profile): add profile model class

---
+++ src/app/models/profile.model.ts
@@ -0,0 +1,9 @@
+// src\app\models\profile.model.ts
+export interface BlogPost {
+  documentID: string;
+  avatarURL: string;
+  createdDate: Date;
+  description: string;
+  displayName: string;
+  email: string;
+}
