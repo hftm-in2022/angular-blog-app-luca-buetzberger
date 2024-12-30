7e36ea3bcbe7d52f3a9dc0ecea91c344546cd57e
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-12-30 16:51:03 +0100

    feat(authentication): add login modal

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,34 +1,47 @@
 // src\app\shared\toolbar\toolbar.component.ts
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { User } from '@angular/fire/auth';
 import { CommonModule } from '@angular/common';
+import { LoginModalComponent } from '../../features/login-modal/login-modal.component';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, LoginModalComponent],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-  user: User | null = null; // Store the user object
+  user: User | null = null; // Local variable to store the user object
+  showLoginModal = false; // Controls the visibility of the login modal
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
-    // Subscribe to the user observable
+    // Subscribe to the user observable and update the local user variable
     this.authService.user$.subscribe((user) => {
       this.user = user;
     });
   }
 
-  loginWithGoogle() {
-    this.authService.loginWithGoogle();
+  openLoginModal() {
+    this.showLoginModal = true;
+  }
+
+  closeLoginModal() {
+    this.showLoginModal = false;
   }
 
   logout() {
     this.authService.logout();
+  }
+
+  goToProfile() {
+    this.router.navigate(['/profile']);
   }
 }
