19eb40fedfb44bc0086c370d9cb6bdd452974f8a
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-04 16:45:26 +0100

    feat(create-blog): add validation for blog title and blog content

--- src/app/shared/validators/shared-validators.component.ts
+++ src/app/shared/validators/shared-validators.component.ts
@@ -2,6 +2,9 @@
 
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
+const minBlogPostTitleLength = 5;
+const minBlogPostContentLength = 5;
+
 export class SharedValidators {
   // Password Validator:
   // At least 8 characters, at least 1 uppercase, at least 1 lowercase, at least 1 special character, no spaces.
@@ -11,6 +14,34 @@ export class SharedValidators {
         /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,256}$/;
       const valid = passwordRegex.test(control.value);
       return valid ? null : { invalidPassword: true };
+    };
+  }
+
+  static blogPostTitle() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value && control.value.length < minBlogPostTitleLength) {
+        return {
+          minlength: {
+            requiredLength: minBlogPostTitleLength,
+            actualLength: control.value.length,
+          },
+        };
+      }
+      return null;
+    };
+  }
+
+  static blogPostContent() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value && control.value.length < minBlogPostContentLength) {
+        return {
+          minlength: {
+            requiredLength: minBlogPostContentLength,
+            actualLength: control.value.length,
+          },
+        };
+      }
+      return null;
     };
   }
 }
