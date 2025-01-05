79cede43ce3274abb1b89a0d196932cd7521ba52
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-05 13:35:23 +0100

    feat(error-handling): generate error-handler component

---
+++ src/app/services/error-handler/error-handler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-handler.component.html',
+  styleUrl: './error-handler.component.scss',
+})
+export class ErrorHandlerComponent {}
