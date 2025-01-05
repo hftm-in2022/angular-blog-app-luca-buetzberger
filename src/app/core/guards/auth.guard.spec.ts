be2eb48c4b8a8cd1f7cd155e9b37d9802efa495b
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-05 15:53:28 +0100

    refactor: move guards from guards to core/guards

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,4 +1,4 @@
-// src\app\guards\auth.guard.spec.ts
+// src\app\core\guards\auth.guard.spec.ts
 
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
@@ -6,8 +6,7 @@ import { CanActivateFn } from '@angular/router';
 import { authGuard } from './auth.guard';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) => TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
