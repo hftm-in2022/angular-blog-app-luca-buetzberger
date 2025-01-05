8ef79d122d8953da627161ee04e82a8f599df010
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-05 15:00:52 +0100

    feat(error-handling): add router to error handler page to direct back to home

--- src/app/shared/error-page/error-page.component.ts
+++ src/app/shared/error-page/error-page.component.ts
@@ -1,11 +1,12 @@
 // src\app\shared\error-page\error-page.component.ts
 
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.scss',
 })
