e21e5036c49310e792c4263f573602c867685825
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-05 14:38:07 +0100

    feat(error-handler): add global error handler service

---
+++ src/app/shared/error-page/error-page.component.ts
@@ -0,0 +1,12 @@
+// src\app\shared\error-page\error-page.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-page.component.html',
+  styleUrl: './error-page.component.scss',
+})
+export class ErrorPageComponent {}
