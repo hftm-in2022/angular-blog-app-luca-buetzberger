f2d89294b7d5c3c6431e12f03da847b09cbc62dc
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2025-01-27 18:51:58 +0100

    feat(translation): add en for error-page component

--- src/app/shared/error-page/error-page.component.ts
+++ src/app/shared/error-page/error-page.component.ts
@@ -2,11 +2,12 @@
 
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.scss',
 })
